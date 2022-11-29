f30134f93874736953e6862de662ea49acfe7084
Aida1108 <mi.aida@yandex.ru>
2022-11-29 10:19:19 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        System.out.println("Задание 1");
+        int year = 0;
+        int nowYear = 2022;
+        int startYear = nowYear - 200;
+        int finishYear = nowYear + 100;
+        int period = 79;
+        while (year <= finishYear) {
+            if (year >= startYear) {
+                System.out.println(+year);
+            }
+            year += period;
+        }
+        // Задание 2
+        System.out.println("Задание 2");
+        int b = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(b + " * " + i + " = " + (i * b));
+        }
+
+
     }
 }
